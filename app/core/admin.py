7dfd16848e34ac9c9059095ed1aa85e60fd5cce3
Ramin Zamani <ramin@lizard.global>
2024-08-24 17:06:18 -0400

    django admin add user

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from app.core import models
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser')
+        }),
+    )
+
+    readonly_fields = ['last_login']
+
+
+admin.site.register(models.User, UserAdmin)
